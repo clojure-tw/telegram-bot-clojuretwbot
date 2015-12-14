7a46652d20bdcac50775b5af38ad6821ff488ca8
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-12-15 01:04:54 +0800

    fix travis-ci building error
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git clone "https://${GH_TOKEN}@${GH_REF}" $OUT
 (cd $OUT; git checkout -b gh-pages origin/gh-pages ; git rm -rf *)
 
 # Copy all prebuild files
-cp -rf target/doc/* $OUT/
+cp docs/uberdoc.html $OUT/index.html
 
 # go to the out directory and create a *new* Git repo
 cd $OUT
@@ -24,7 +24,7 @@ git init
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
-git config user.email "coldnew.tw@gmail.com"
+git config user.email "clojure.tw@gmail.com"
 
 # We permit following exit with error
 set +e
@@ -32,7 +32,7 @@ set +e
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
 git add .
-git commit -m "deploy commit $ID to GitHub Pages: $DATE"
+git commit -m "$DATE deploy commit $ID"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
