97dffc9b5e37a8f5b562889fc75e7430c6dab86b
Yu-Fu Wu <wennynnewenny@gmail.com>
2016-01-10 23:47:18 +0800

    remove distinct

--- src/clojuretwbot/core.clj
+++ src/clojuretwbot/core.clj
@@ -42,9 +42,9 @@
    (let [atom-feed (feed/parse-feed feed-uri)
          archive (edn/read-string (slurp feed-archive))
          ;; new-item (first (data/diff (map :uri (:entries atom-feed)) archive))
-         new-item (into #{} (map (fn [x] (if (contains? archive x) x nil)) atom-feed))]
+         new-item (into #{} (map (fn [x] (if (contains? archive x) x nil)) atom-feed))3]
      (spit feed-archive
-           (with-out-str (pr (distinct (into #{} (concat new-item archive))))))
+           (with-out-str (pr (into #{} (concat new-item archive)))))
      new-item)))
 
 (defn tweet-to-telegram
