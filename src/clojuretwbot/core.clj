9b19651894f69b0539d1291883793e92fa230013
Yu-Fu Wu <wennynnewenny@gmail.com>
2016-01-10 23:49:36 +0800

    remove mistouch

--- src/clojuretwbot/core.clj
+++ src/clojuretwbot/core.clj
@@ -42,7 +42,7 @@
    (let [atom-feed (feed/parse-feed feed-uri)
          archive (edn/read-string (slurp feed-archive))
          ;; new-item (first (data/diff (map :uri (:entries atom-feed)) archive))
-         new-item (into #{} (map (fn [x] (if (contains? archive x) x nil)) atom-feed))3]
+         new-item (into #{} (map (fn [x] (if (contains? archive x) x nil)) atom-feed))]
      (spit feed-archive
            (with-out-str (pr (into #{} (concat new-item archive)))))
      new-item)))
