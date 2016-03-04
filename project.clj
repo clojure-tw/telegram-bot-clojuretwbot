77aa444b2c9ab8519d9b974f44a7f94c639e9a99
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-03-04 22:37:26 +0800

    update dependancies
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- project.clj
+++ project.clj
@@ -5,14 +5,15 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.374"]
-                 [clj-http "2.0.0"]
+                 [clj-http "2.1.0"]
                  [cheshire "5.5.0"]
                  [org.clojars.scsibug/feedparser-clj "0.4.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [im.chit/cronj "1.4.3"]
+                 [im.chit/cronj "1.4.4"]
                  [com.cemerick/url "0.1.1"]
-                 [com.taoensso/timbre "4.2.0"]
-                 [datascript "0.13.3"]]
+                 [com.taoensso/timbre "4.3.1"]
+                 [yesql "0.5.2"]
+                 [org.xerial/sqlite-jdbc "3.8.11.2"]]
 
   :plugins [[michaelblume/lein-marginalia "0.9.0"]]
   :main ^:skip-aot clojuretwbot.core)
