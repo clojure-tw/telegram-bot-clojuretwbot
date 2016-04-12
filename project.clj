c240d0126d8928f6683e7a5a95dc4c8f8642f3ef
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-04-12 15:41:17 +0800

    Revert "Fix out of date dependencies"
    Wait for more stable to join
    
    This reverts commit 0f95ca492658e3e479792d69f83e6c781d33a230.

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
-                 [clj-http "3.0.0"]
-                 [cheshire "5.6.0"]
+                 [clj-http "2.1.0"]
+                 [cheshire "5.5.0"]
                  [org.clojars.scsibug/feedparser-clj "0.4.0"]
                  [org.clojure/data.json "0.2.6"]
                  [com.cemerick/url "0.1.1"]
